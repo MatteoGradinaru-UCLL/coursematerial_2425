c6594e0ee7c8dedec3a39c9417a018f31b1db1c2
Matteo Gradionaru <matteo.gradinaru@student.ucll.be>
2024-11-26 14:26:39 +0100

    OBJECTS DONE

--- 12-objects/15-assignment-tweet/student.py
+++ 12-objects/15-assignment-tweet/student.py
@@ -1,7 +1,41 @@
 class Tweet:
     def __init__(self, message, max_length):
-        self.__max_length = max_length
         self.message = message
+        self.max_length = max_length
+    
+    @property
+    def max_length(self):
+        return self.__max_length
+    
+    @max_length.setter
+    def max_length(self, value):
+        if value <= 0:
+            raise ValueError("max lenght moet groter zijn dan 0")
+        self.__max_length = value
+
+    @property
+    def message(self):
+        if len(self.__message) > self.max_length:
+            return self.__message[:self.max_length]
+        return self.__message
+
+    @message.setter
+    def message(self,value):
+        self.__message = value
+
+tweet = Tweet("abc", 10)
+print(tweet.message)
+
+tweet.max_length = 1
+print(tweet.message)
+
+
+
+'''class Tweet:
+    def __init__(self, message, max_length):
+        self.message = message
+        self.max_length = max_length
+        
 
     @property
     def message(self):
@@ -28,3 +62,4 @@ class Tweet:
 
 my_tweet = Tweet("1234567", 5)
 print(my_tweet.message)
+'''
